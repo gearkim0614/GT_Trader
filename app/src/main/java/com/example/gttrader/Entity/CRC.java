117daaeec398ea6883679107231ffe1ccc9b4959
Kibeom Kim <gearkim0614@gmail.com>
2019-06-20 18:20:52 -0400

    please work

--- app/src/main/java/com/example/gttrader/Entity/CRC.java
+++ app/src/main/java/com/example/gttrader/Entity/CRC.java
@@ -1,4 +1,16 @@
 package com.example.gttrader.Entity;
 
 public class CRC extends Region {
+
+    public CRC(String name, double latitude, double longitude, Resource resource, TechLevel techLevel) {
+        super("CRC", 33.775627, -84.403491, Resource.LOTSOFWATER, TechLevel.INDUSTRIAL);
+    }
+
+//    public CRC(String name, double latitude, double longitude, TechLevel techLevel) {
+//        super(name, latitude, longitude, techLevel);
+//    }
+//
+//    public CRC(String name, double latitude, double longitude) {
+//        super(name, latitude, longitude);
+//    }
 }
