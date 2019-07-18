1411d19d49e2cf28d9deb3f2a173e4b00ff48e77
Jason <jau8@gatech.edu>
2019-07-18 18:11:49 -0400

    Persistence

--- app/src/main/java/com/example/gttrader/MainActivity.java
+++ app/src/main/java/com/example/gttrader/MainActivity.java
@@ -5,40 +5,36 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.content.Intent;
 
+import com.example.gttrader.Entity.Building;
 import com.example.gttrader.Entity.DifficultyLevel;
 import com.example.gttrader.Entity.Universe;
-import com.example.gttrader.View.Main2Activity;
-import com.example.gttrader.ViewModel.ConfigurationViewModel;
+import com.example.gttrader.View.ConfigurationActivity;
 import com.example.gttrader.Entity.Player;
+import com.example.gttrader.Entity.Scooter;
+import com.example.gttrader.View.Main2Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.google.gson.Gson;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
-    EditText playerNameText;
-    EditText pilotPoints;
-    EditText engineerPoints;
-    EditText fighterPoints;
-    EditText traderPoints;
-    Spinner levelSpinner;
+    TextView title;
+    Button start_gameButton;
+    Button saveExitButton;
 
-
-    Button start_button;
-    Button quit_button;
-
-    private ConfigurationViewModel configuration = new ConfigurationViewModel();
+    //private ConfigurationViewModel configuration = new ConfigurationViewModel();
 
     //private Player player;
-    private Gson gson;
+    private Gson gson= new Gson();
+    private Gson gson2 = new Gson();
     private Universe universe = Universe.getUniverse();
 
 
@@ -47,25 +43,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        start_button = findViewById(R.id.startButton);
-        quit_button = findViewById(R.id.quitButton);
-        playerNameText = findViewById(R.id.PlayerNameText);
-        pilotPoints = findViewById(R.id.pilot_points);
-        engineerPoints = findViewById(R.id.engineer_points);
-        fighterPoints = findViewById(R.id.fighter_points);
-        traderPoints = findViewById(R.id.trader_points);
-        levelSpinner = findViewById(R.id.difficultySpinner);
+        start_gameButton = findViewById(R.id.start_gameButton);
+        saveExitButton = findViewById (R.id.saveExitButton);
 
-        ArrayAdapter<DifficultyLevel> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, DifficultyLevel.values());
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        levelSpinner.setAdapter(adapter);
+        start_gameButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) { // button clicked
+                startActivity (new Intent(MainActivity.this, ConfigurationActivity.class) );
 
+            }
+        });
+
+        saveExitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) { // button clicked
+                //make object and JSON
+                System.out.println(universe.getPlayer ());
+                if (universe.getPlayer () == null) {
+                    saveExitButton.setEnabled ( false );
+                }
+
+
+                String pjson = gson.toJson(universe.getPlayer());
+                String sjson = gson2.toJson(universe.getPlayer().getScooter ());
+
+
+                File path = getApplicationContext().getFilesDir();
+                File file = new File(path, "PlayerInfo.json");
+                File file2 = new File(path, "ScooterInfo.json");
+
+                //write to file
+                try {
+                    FileOutputStream stream = new FileOutputStream(file);
+                    stream.write(pjson.getBytes());
+                    stream.close();
+                    FileOutputStream stream2 = new FileOutputStream (file2);
+                    stream2.write(sjson.getBytes());
+                    stream2.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+                finish();
+                System.exit ( 0 );
+            }
+        });
 
         //read file
         File path = getApplicationContext().getFilesDir();
-        File file = new File(path, "Info.json");
+        File file = new File(path, "PlayerInfo.json");
+        File file2 = new File(path, "ScooterInfo.json");
         try {
             int length = (int) file.length();
             byte[] bytes = new byte[length];
@@ -73,13 +99,22 @@ public class MainActivity extends AppCompatActivity {
             in.read(bytes);
             String contents = new String(bytes);
             in.close();
+
+            int length2 = (int) file2.length();
+            byte[] bytes2 = new byte[length2];
+            FileInputStream in2 = new FileInputStream(file2);
+            in2.read(bytes2);
+            String contents2 = new String(bytes2);
+            in2.close();
 //            Log.d("FileFromLastInstance", contents);
             if (!contents.equals("")) {
                 System.out.println("here");
                 Player player = gson.fromJson(contents, Player.class);
+                Scooter scooter = gson2.fromJson(contents2, Scooter.class);
                 universe.setPlayer(player);
-                startActivity (new Intent(MainActivity.this, Main2Activity.class) );
-                Toast.makeText(getApplicationContext(), "Welcome!", Toast.LENGTH_LONG).show();
+                player.setScooter ( scooter );
+                startActivity (new Intent( MainActivity.this, Main2Activity.class) );
+                Toast.makeText (getApplicationContext (), "Welcome!", Toast.LENGTH_LONG ).show ();
 //                previousStateReviewText.setText(previousStateReview.getReview());
 //                previousStateRatingText.setText(previousStateReview.getRating() + "");
             }
@@ -87,34 +122,6 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        start_button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) { // button clicked
-                String name = playerNameText.getText().toString();
-                int pilot_pts = Integer.parseInt(pilotPoints.getText().toString());
-                int engineer_pts = Integer.parseInt(engineerPoints.getText().toString());
-                int fighter_pts = Integer.parseInt(fighterPoints.getText().toString());
-                int trader_pts = Integer.parseInt(traderPoints.getText().toString());
-                DifficultyLevel level = (DifficultyLevel) levelSpinner.getSelectedItem();
-                if (!ConfigurationViewModel.addUpToSixteen(pilot_pts, engineer_pts, fighter_pts, trader_pts)) {
-                    Toast.makeText(getApplicationContext(), "Invalid skill points", Toast.LENGTH_LONG).show();
-                }
-
-                else {
-                    configuration.initializer(pilot_pts, engineer_pts, fighter_pts, trader_pts, name);
-                    startActivity(new Intent(MainActivity.this, Main2Activity.class));
-                    Toast.makeText(getApplicationContext(), "Welcome!", Toast.LENGTH_LONG).show();
-
-                }
-            }});
-
-        quit_button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) { // button clicked
-                finish();
-
-
-            }});
-
 
 
     }
