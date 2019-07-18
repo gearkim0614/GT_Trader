3e028448b9c6a6e634658cd6a7861de2287604ec
PeggyZhao2 <peggy-zhao@outlook.com>
2019-07-18 17:19:05 -0400

    three random events

--- app/src/test/java/com/example/gttrader/PeggyJUnite.java
+++ app/src/test/java/com/example/gttrader/PeggyJUnite.java
@@ -1,5 +1,52 @@
-package com.example.gttrader;
-
-public class PeggyJUnite {
-    //assert();
-}
+//package com.example.gttrader;
+//import android.os.Build;
+//
+//import com.example.gttrader.Entity.Building;
+//import com.example.gttrader.Entity.Player;
+//import com.example.gttrader.Entity.Scooter;
+//import com.example.gttrader.Entity.Universe;
+//import com.example.gttrader.ViewModel.ConfigurationViewModel;
+//import com.example.gttrader.ViewModel.TravelViewModel;
+//
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//import com.example.gttrader.Entity.Universe;
+//
+//
+//
+//import static org.junit.Assert.*;
+//
+//public class PeggyJUnite {
+//    private ConfigurationViewModel configurationViewModel = new ConfigurationViewModel();
+//    private TravelViewModel travelViewModel = new TravelViewModel();
+//    private Universe universe = Universe.getUniverse();
+//    private Player player = universe.getPlayer();
+//
+//    @Test
+//    public void testCanTravel() {
+//        PeggyJUnite peggyJUnite = new PeggyJUnite();
+////        configurationViewModel.initializer(4,4,4,4, "name");
+////        Player myPlayer = configurationViewModel.getPlayer();
+////        myPlayer.setScooter(new Scooter());
+//        Player player = new Player("Player", 4, 4, 4, 4);
+//        player.setScooter(new Scooter());
+//
+////        travelViewModel.setPlayer(player);
+//        assertTrue(travelViewModel.canTravel(Building.BOBBYDODD, Building.COC));
+//       // assert
+//    }
+//
+//    @Test
+//    public void testrobbed() {
+//        Player player = new Player("playerTest2", 4, 4, 4, 4);
+//        //player.setScooter(new Scooter());
+//        universe.setPlayer(player);
+//        //configurationViewModel = new Confi
+//
+//        //travelViewModel.setPlayer(player);
+//        //assertEquals(900, configurationViewModel.robbed());
+//        assertEquals(900, configurationViewModel.robbed());
+//    }
+//
+////    assertTrue(travelViewModel.canTravel(Building.COC, Building.CRC));
+//}
